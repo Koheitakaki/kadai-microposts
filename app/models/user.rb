f85203193923d5f833cf2d88675c4947e1ff0d8d
kohei.LD <NA>
2018-04-24 08:42:19 +0000

    created favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,17 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :favorites source: :micropost
+  
+  def bookmark(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unbookmark(target_micropost)
+    preference = self.favorites.find_by(micropost_id: micropost.id)
+    preference.destroy if preference
+  end
 
   def follow(other_user)
     unless self == other_user
